af77998672c93b115fefbc2b47027ec8b858ff78
Nownitya <nownitya@gmail.com>
2023-12-24 09:06:59 +0530

    Initial

---
+++ app/src/main/java/com/nowni/jetpackcomposeproject/tip_time_starter/ui/theme/Theme.kt
@@ -0,0 +1,104 @@
+package com.nowni.jetpackcomposeproject.tip_time_starter.ui.theme
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = dark_primary,
+    onPrimary = dark_onPrimary,
+    primaryContainer = dark_primaryContainer,
+    onPrimaryContainer = dark_onPrimaryContainer,
+    secondary = dark_secondary,
+    onSecondary = dark_onSecondary,
+    secondaryContainer = dark_secondaryContainer,
+    onSecondaryContainer = dark_onSecondaryContainer,
+    tertiary = dark_tertiary,
+    onTertiary = dark_onTertiary,
+    tertiaryContainer = dark_tertiaryContainer,
+    onTertiaryContainer = dark_onTertiaryContainer,
+    error = dark_error,
+    onError = dark_onError,
+    errorContainer = dark_errorContainer,
+    onErrorContainer = dark_onErrorContainer,
+    background = dark_background,
+    onBackground = dark_onBackground,
+    surface = dark_surface,
+    onSurface = dark_onSurface,
+    surfaceVariant = dark_surfaceVariant,
+    onSurfaceVariant = dark_onSurfaceVariant,
+    outline = dark_outline,
+    inverseSurface = dark_inverseSurface,
+    inverseOnSurface = dark_inverseOnSurface,
+    inversePrimary = dark_inversePrimary,
+    surfaceTint = dark_surfaceTint,
+    outlineVariant = dark_outlineVariant,
+    scrim = dark_scrim
+    )
+
+private val LightColorScheme = lightColorScheme(
+    primary = light_primary,
+    onPrimary = light_onPrimary,
+    primaryContainer = light_primaryContainer,
+    secondary = light_secondary,
+    onSecondary = light_onSecondary,
+    secondaryContainer = light_secondaryContainer,
+    onSecondaryContainer = light_onSecondaryContainer,
+    tertiary = light_tertiary,
+    onTertiary = light_onTertiary,
+    tertiaryContainer = light_tertiaryContainer,
+    onTertiaryContainer = light_onTertiaryContainer,
+    error = light_error,
+    errorContainer = light_errorContainer,
+    background = light_background,
+    onBackground = light_onBackground,
+    surface = light_surface,
+    surfaceVariant = light_surfaceVariant,
+    onSurface = light_onSurface,
+    onSurfaceVariant = light_onSurfaceVariant,
+    outline = light_outline,
+    inverseSurface = light_inverseSurface,
+    inverseOnSurface = light_inverseOnSurface,
+    inversePrimary = light_inversePrimary,
+    surfaceTint = light_surfaceTint,
+    outlineVariant = light_outlineVariant,
+    scrim = light_scrim
+    )
+
+@Composable
+fun TipTimeInitialTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+//    val view = LocalView.current
+//    if (!view.isInEditMode) {
+//        SideEffect {
+//            val window = (view.context as Activity).window
+//            window.statusBarColor = colorScheme.primary.toArgb()
+//            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+//        }
+//    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
